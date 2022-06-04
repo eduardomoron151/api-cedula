46c4a996a480680960d91fb16962381c7c2a1052
eduardo moron <eduardomoron151@gmail.com>
2022-06-04 15:15:35 -0400

    actualizacion fetchHTML

--- src/helpers/fetch-html.js
+++ src/helpers/fetch-html.js
@@ -3,7 +3,11 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const fetchHTML = async(url) => {
-    const { data } = await axios.get(url)
+
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16'
+    }
+    const { data } = await axios.get(url, headers)
     return cheerio.load(data)
 }
 
