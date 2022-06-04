504454bf01c8383d711a4bf46505dcf36c7ec184
eduardo moron <eduardomoron151@gmail.com>
2022-06-04 17:22:39 -0400

    actualizacion y creacion de middleware para verificar campos

--- src/routes/buscar.routes.js
+++ src/routes/buscar.routes.js
@@ -3,10 +3,15 @@ const { check } = require('express-validator');
 
 
 const { buscarCedula } = require('../controllers/buscar.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
-router.get('/:nacionalidad/:cedula',  buscarCedula);
+router.get('/:nacionalidad/:cedula', [
+    check('nacionalidad', 'La nacionalidad es V o E').isIn(['V', 'E']),
+    check('cedula', 'La cedula debe ser mayor a 6 caracteres').isLength({min : 6}),
+    validarCampos
+], buscarCedula);
 
 module.exports = router;
 
